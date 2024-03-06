b8fdd13e25725b9d01c3afae5b53a9ebaf119f21
WereAsh <wereashhh@gmail.com>
2024-03-06 17:21:26 +0800

    建立用户查询缓存

--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 /**
  * <p>
@@ -17,4 +26,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryById(Long id) {
+        String shopKey= CACHE_SHOP_KEY+id;
+        String shopJson = stringRedisTemplate.opsForValue().get(shopKey);
+        if(StrUtil.isNotBlank(shopJson)){
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        Shop shop=getById(id);
+        if(shop==null){
+            return Result.fail("店铺不存在");
+        }
+
+        stringRedisTemplate.opsForValue().set(shopKey,JSONUtil.toJsonStr(shop));
+        return Result.ok(shop);
+    }
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,24 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -17,4 +31,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryShopList() {
+
+        List<String> shopTypesJson = stringRedisTemplate.opsForList().range(CACHE_SHOP_TYPE_KEY, 0, -1);
+
+        if (CollectionUtil.isNotEmpty(shopTypesJson)) {
+            List<ShopType> shopTypes = shopTypesJson.stream()
+                    .map(item -> JSONUtil.toBean(item, ShopType.class))
+                    .sorted(Comparator.comparingInt(ShopType::getSort))
+                    .collect(Collectors.toList());
+            return Result.ok(shopTypes);
+        }
+        List<ShopType> shopTypes = query().orderByAsc("sort").list();
+
+        if(CollectionUtil.isEmpty(shopTypes)){
+            //不存在则返回一个空集合，解决缓存穿透
+
+            stringRedisTemplate.opsForList().rightPushAll(CACHE_SHOP_TYPE_KEY,Collections.emptyList());
+            stringRedisTemplate.expire(CACHE_SHOP_TYPE_KEY,CACHE_SHOP_TTL,TimeUnit.MINUTES);
+
+            return Result.fail("商铺数据为空！");
+        }
+
+
+        shopTypesJson = shopTypes.stream()
+                .map(JSONUtil::toJsonStr)
+                .collect(Collectors.toList());
+
+        //此处使用右插入保存顺序性
+        stringRedisTemplate.opsForList().rightPushAll(CACHE_SHOP_TYPE_KEY,shopTypesJson);
+        stringRedisTemplate.expire(CACHE_SHOP_TYPE_KEY,CACHE_SHOP_TTL,TimeUnit.MINUTES);
+
+        return Result.ok(shopTypes);
+    }
 }
