75a621d52d5d99ab3e5cbde5003b1c18a43c376e
WereAsh <wereashhh@gmail.com>
2024-03-23 23:50:28 +0800

    关注功能

--- hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,15 @@
 package com.hmdp.controller;
 
 
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -16,5 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+
+    @Resource
+    IFollowService followService;
+
+    @GetMapping("/{id}/{isFollow}")
+    public Result follow(@PathVariable("id") Long followUserId,@PathVariable("isFollow") Boolean isFollow){
+        return followService.follow(followUserId,isFollow);
+    }
+
+    public Result isFollow(@PathVariable("id") Long followUserId){
+        return followService.isFollow(followUserId);
+    }
 
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import static com.hmdp.utils.RedisConstants.FOLLOW_KEY;
 
 /**
  * <p>
@@ -17,4 +26,39 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        Long userId = UserHolder.getUser().getId();
+        String key=FOLLOW_KEY+userId;
+        //判断是关注还是取关
+        if(isFollow){
+            Follow follow = new Follow();
+            follow.setFollowUserId(followUserId);
+            follow.setUserId(userId);
+            boolean isSuccess = save(follow);
+            if(isSuccess){
+                stringRedisTemplate.opsForSet().add(key,followUserId.toString());
+            }
+
+        }else {
+            //取关
+            boolean isSuccess = remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId).eq("follow_id", followUserId));
+            if(isSuccess){
+                stringRedisTemplate.opsForSet().remove(key,followUserId.toString());
+            }
+        }
+        return  Result.ok();
+    }
+
+    @Override
+    public Result isFollow(Long followUserId) {
+        Long userId = UserHolder.getUser().getId();
+
+        Integer count = query().eq("user_id", userId).eq("follow_id", followUserId).count();
+
+        return Result.ok(count>0);
+    }
 }
